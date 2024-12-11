ed35ab0efeb15371ba573cb39ec4233d0511bc1e
AssiarDev <halibaraissa@gmail.com>
2024-12-11 17:00:14 +0100

    ajout d'une table users etape 2

--- edusign.sql
+++ edusign.sql
@@ -2,7 +2,7 @@
 CREATE TABLE edusign (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     firstname TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE, 
+    email TEXT NOT NULL, 
     created_ad DATETIME NOT NULL
 )
 
@@ -15,10 +15,17 @@ INSERT INTO edusign (firstname, email, created_ad)
 INSERT INTO edusign (firstname, email, created_ad)
     VALUES ('Ella', 'ella@adatechschool.com', '2024-05-08 09:30:00');
 INSERT INTO edusign (firstname, email, created_ad)
-    VALUES ('Grace', 'grace@adatechschool.com', '2024-05-08 09:30:00');
+    VALUES ('Grace', 'grace@adatechschool.com', '2024-05-09 09:30:00');
 INSERT INTO edusign (firstname, email, created_ad)
-    VALUES ('Ella', 'ella@adatechschool.com', '2024-05-08 09:30:00');
+    VALUES ('Ella', 'ella@adatechschool.com', '2024-05-09 09:30:00');
 
 
 SELECT * FROM edusign;
+
+CREATE TABLE users (
+    prenom TEXT NOT NULL,
+    nom TEXT NOT NULL, 
+    email TEXT NOT NULL
+);
+
 
